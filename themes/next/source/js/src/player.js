c61e1467b582597ba8f3cf5767c1cbc90fb0b451
AaronW <zmtzwb@gmail.com>
2019-02-25 03:12:05 +0800

    loop to true

--- themes/next/source/js/src/player.js
+++ themes/next/source/js/src/player.js
@@ -7,5 +7,6 @@ $(document).ready(function () {
   let audio = document.createElement('audio');
   audio.src = playList[0];
   audio.autoplay = true;
+  audio.loop = true;
   document.body.appendChild(audio);
 });
